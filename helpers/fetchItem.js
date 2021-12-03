45d3e09caeb6eac60db27b58144268097edcae25
vitorcorrea18 <vitorcorrea18@gmail.com>
2021-12-03 19:08:18 -0400

    requisito 2 => concluido a funçao fetchItem junto com o requisito 9, feito em TDD

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,12 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (id) => {
+  const url = `https://api.mercadolibre.com/items/${id}`;
+
+  try {
+    const item = await (await fetch(url)).json();
+    return item;
+  } catch (error) {
+    return error;
+  }
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,32 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fecthItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Teste se fetchItem é uma função;', () => {
+    const actual = typeof fetchItem;
+    const expected = 'function';
+    expect(actual).toBe(expected);
+  })
+
+  it('Executa a função fetchItem com o argumento do item "MLB1615760527" e testa se fetch foi chamada', () => {
+    fetchItem("MLB1615760527");
+    expect(fetch).toHaveBeenCalled();
+  })
+
+  it('Testa se, ao chamar a função fetchItem com o argumento do item "MLB1615760527", a função fetch utiliza o endpoint', () => {
+    const endPoint = "https://api.mercadolibre.com/items/MLB1615760527";
+    fetchItem("MLB1615760527");
+    expect(fetch).toHaveBeenCalledWith(endPoint);
+  })
+
+  it('Testa se o retorno da função fetchItem com o argumento do item "MLB1615760527" é uma estrutura de dados igual ao objeto item', async () => {
+    const actual = await fetchItem("MLB1615760527");
+    const expected = item;
+    expect(actual).toEqual(expected);
+  })
+
+  it('Testa se, ao chamar a função fetchItem sem argumento, retorna um erro com a mensagem: You must provide an url', async () => {
+    const error = new Error('You must provide an url')
+    const actual = await fetchItem();
+    expect(actual).toEqual(error);
+  })
 });
