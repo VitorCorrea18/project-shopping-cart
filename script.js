97cf0aa0ba464506936fabc8c070aabe2a52fbdc
vitorcorrea18 <vitorcorrea18@gmail.com>
2021-12-02 20:12:58 -0400

    requisito 1 e 8 concluidos

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -40,4 +40,10 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-window.onload = () => { };
+window.onload = async () => {
+  const data = await fetchProducts('computador');
+  const { results } = await data;
+  const itemSection = document.querySelector('.items');
+  console.log(results);
+  results.forEach((element) => itemSection.appendChild(createProductItemElement(element)));
+ };
