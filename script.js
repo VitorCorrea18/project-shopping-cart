02640f47ad06d06c5d3d1eae0d25b76591bbdb8c
vitorcorrea18 <vitorcorrea18@gmail.com>
2021-12-03 21:54:29 -0400

    requsitos 2 e 9 concluidos

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function cartItemClickListener(event) {
   // coloque seu código aqui
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
@@ -40,14 +40,36 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
+// adiciona um event listener em cada botão "adicionar ao carrinho", que procura o id do item clicado
+// usando a função getSkuFromProductItem e dando como argumento o parentNode do item, que é a section em que
+// se encontra os dados do produto. Com o itemId uso a função (helper)fetchItem para buscar o objeto do produto
+// que será passado como parâmetro para a função createCartItemElement.
+function addItemClickListener() {
+  const buttons = document.querySelectorAll('.item__add');
+  const cartSection = document.querySelector('.cart__items');
+
+  buttons.forEach((button) => button.addEventListener('click', async ({ target }) => {
+    const itemID = getSkuFromProductItem(target.parentNode);
+    cartSection.appendChild(createCartItemElement(await fetchItem(itemID)));
+  }));
+}
+
+// a função fetchedData busca de forma assincrona as informação do produto buscado (no caso 'computador'),
+// o array desestruturado de data 'results' é onde estará todos os produtos retornados pela API.
+// Ao rodar um loop (forEach) no array results, se insere na itemSection cada produto usando a função 'createProductItemElement'
+// dentro da HOF 'appendChild'. 
 const fetchedData = async () => {
   const data = await fetchProducts('computador');
   const { results } = await data;
   const itemSection = document.querySelector('.items');
-  console.log(results);
   results.forEach((element) => itemSection.appendChild(createProductItemElement(element)));
 };
 
+const addCart = async () => {
+
+};
+
 window.onload = async () => {
-  fetchedData();
+  await fetchedData();
+  addItemClickListener();
  };
